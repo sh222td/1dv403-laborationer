8229092dae244a0ba237e7e0d4a4667e8776a4c7
OpenShift guest <sh222td@student.lnu.se>
2013-11-20 09:59:48 -0500

    Lab 1,4 färdig

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,38 @@
 
 var makePerson = function(persArr){
 
-
-	// Din kod här...
-
-}
-
+    var personObj = {};
+    var ages = [];
+    var averageAge;
+    var names = [];
+    
+    
+    ages = persArr.map(function(person){
+        return person.age;
+    });
+    
+    personObj.maxAge = ages.reduce(function(prevAge, age){
+        return Math.max(prevAge, age);
+    });
+    
+    personObj.minAge = ages.reduce(function(prevAge, age){
+        return Math.min(prevAge, age);
+    });
+    
+    personObj.averageAge = Math.round((persArr[0].age + persArr[1].age + persArr[2].age) / 3);
+    
+    names = persArr.map(function(person) {
+       return person.name; 
+    });
+    
+    personObj.names = names.sort(function(a, b){
+            return a.localeCompare(b);
+    });
+    
+    personObj.names = names.reduce(function(names, name) {
+        return names + ", " + name;
+    });
+ 
+    return personObj;
+    
+};
